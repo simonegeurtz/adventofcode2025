e2af67ef82f157b282fdc5bc00c164de02e85fc6
Simone Geurtz <simone.geurtz@cito.nl>
2025-12-04 18:17:34 +0100

    dag 4

---
+++ adventofcode/Program - dag 4.cs
@@ -0,0 +1,212 @@
+﻿Console.WriteLine("Hello, World!");
+
+var input = File.ReadAllLines("input.txt");
+
+int amountofpaper = 0;
+for (var line = 0; line < input.Length; line++)
+{
+    int amounttoAdd = 0;
+
+    for (var i = 0; i < input[line].Length; i++)
+    {
+        var amountsurrounding = 0;
+
+        if (input[line][i] != '@')
+        {
+            continue;
+        }
+
+        //rij 1
+        else if (line == 0)
+        {
+            //de meest linkste
+            if (i == 0)
+            {
+                bool[] allsurrounding = [Onder(input, line, i), Rechtsonder(input, line, i), Rechts(input, line, i)];
+                foreach (var a in allsurrounding)
+                { 
+                    if (a == true)
+                    {
+                        amountsurrounding++;
+                    }
+                }
+               
+            }
+            //de meest rechtse
+            else if (i == input[line].Length - 1)
+            {
+                bool[] allsurrounding = [Onder(input, line, i), Linksonder(input, line, i), Links(input, line, i)];
+                foreach (var a in allsurrounding)
+                {
+                    if (a == true)
+                    {
+                        amountsurrounding++;
+                    }
+                }
+            }
+            //alle middelste
+            else
+            {
+                bool[] allsurrounding = [Onder(input, line, i), Rechtsonder(input, line, i), Rechts(input, line, i), Links(input, line, i), Linksonder(input, line, i)];
+                foreach (var a in allsurrounding)
+                {
+                    if (a == true)
+                    {
+                        amountsurrounding++;
+                    }
+                }
+            }
+
+        }
+
+
+        //rij onderaan
+        else if (line == input.Length - 1)
+        {
+            //de meest linkse
+            if (i == 0)
+            {
+                bool[] allsurrounding = [Boven(input, line, i), Rechtsboven(input, line, i), Rechts(input, line, i)];
+                foreach (var a in allsurrounding)
+                {
+                    if (a == true)
+                    {
+                        amountsurrounding++;
+                    }
+                }
+            }
+
+            //de meest rechtste
+            else if (i == input[line].Length - 1)
+            {
+                //je wil boven, linksboven, links
+                bool[] allsurrounding = [Boven(input, line, i), Linksboven(input, line, i), Links(input, line, i)];
+                foreach (var a in allsurrounding)
+                {
+                    if (a == true)
+                    {
+                        amountsurrounding++;
+                    }
+                }
+            }
+
+            //alle middelste
+            else
+            {
+                bool[] allsurrounding = [Linksboven(input, line, i), Boven(input, line, i), Rechtsboven(input, line, i), Links(input, line, i), Rechts(input, line, i)];
+                foreach (var a in allsurrounding)
+                {
+                    if (a == true)
+                    {
+                        amountsurrounding++;
+                    }
+                }
+            }
+        }
+
+
+        //alle middelste rijen 
+        else
+        {
+            //de meest linkse
+            if (i == 0)
+            {
+                bool[] allsurrounding = [Onder(input, line, i), Boven(input, line, i), Rechtsboven(input, line, i), Rechtsonder(input, line, i), Rechts(input, line, i)];
+                foreach (var a in allsurrounding)
+                {
+                    if (a == true)
+                    {
+                        amountsurrounding++;
+                    }
+                }
+            }
+
+            //de meest rechtse
+
+            else if (i == input[line].Length - 1)
+            {
+
+                bool[] allsurrounding = [Onder(input, line, i), Boven(input, line, i), Linksboven(input, line, i), Linksonder(input, line, i), Links(input, line, i)];
+                foreach (var a in allsurrounding)
+                {
+                    if (a == true)
+                    {
+                        amountsurrounding++;
+                    }
+                }
+            }
+
+            //alle middelste
+            else
+            {
+                //je wil linksboven, boven, rechtsboven, links, rechts, linksonder, rechtsonder
+
+                bool[] allsurrounding = [Onder(input, line, i), Linksboven(input, line, i), Rechtsboven(input, line, i), Links(input, line, i), Rechts(input, line, i), Linksonder(input, line, i), Rechtsonder(input, line, i), Boven(input, line, i)];
+                foreach (var a in allsurrounding)
+                {
+                    if (a == true)
+                    {
+                        amountsurrounding++;
+                    }
+                }
+            }
+        }
+
+
+        //if amountsurrounding < 4 dan amount of paper 1 meer
+        if (amountsurrounding < 4)
+        {
+            amountofpaper++;
+            amounttoAdd++;
+            var chars = input[line].ToCharArray();
+            chars[i] = '#';
+            input[line] = new string(chars);
+        }
+    }
+    if(amounttoAdd > 0)
+    {
+        line = -1;
+    }
+
+}
+
+Console.WriteLine($"Number of paper: {amountofpaper}");
+
+static bool Rechts(string[] input, int line, int index)
+{
+    return input[line][index+1].ToString() == "@";
+}
+static bool Links(string[] input, int line, int index)
+{
+    return input[line][index - 1].ToString() == "@";
+}
+
+static bool Linksboven(string[] input, int line, int index)
+{
+    return input[line -1][index - 1].ToString() == "@";
+}
+static bool Rechtsboven(string[] input, int line, int index)
+{
+    return input[line -1][index + 1].ToString() == "@";
+}
+static bool Rechtsonder(string[] input, int line, int index)
+{
+    return input[line +1][index + 1].ToString() == "@";
+}
+static bool Linksonder(string[] input, int line, int index)
+{
+    return input[line +1][index - 1].ToString() == "@";
+}
+static bool Onder(string[] input, int line, int index)
+{
+      var test=  input[line +1][index].ToString();
+    return (test == "@");
+
+}
+static bool Boven(string[] input, int line, int index)
+{
+    return input[line -1][index].ToString() == "@";
+}
+
+
+
--- adventofcode/Program.cs
+++ adventofcode/Program.cs
@@ -2,28 +2,101 @@
 
 var input = File.ReadAllLines("testdata.txt");
 
-long joltage = 0;
-
-foreach (var line in input)
+int amountofpaper = 0;
+for (var line = 0; line < input.Length -1; line++)
 {
-    var joltagestring = "";
-
-    var numbers = line.Select(c => long.Parse(c.ToString())).ToList();
-
-    var result = new List<long>();
-    int start = 0;
-    for (int i = 0; i < 12; i++)
+    for(var i  = 0; i < input[line].Length -1; i++)
     {
-        int maxReach = numbers.Count - (11 - i);
-        long biggestnr = numbers.GetRange(start, maxReach - start).Max();
-        int biggestindex = numbers.IndexOf(biggestnr, start);
+        if (input[line][i].ToString() != "@")
+        {
+            break ;
+        }
+        int amountsurrounding = 0;
 
-        result.Add(biggestnr);
-        joltagestring += biggestnr.ToString();
-        start = biggestindex + 1;
+        var lineBoven = line - 1;
+        var lineOnder = line + 1;
+        var indexLinks = i - 1;
+        var indexRechts = i + 1;
+
+        
+        if (line != 0)
+        {
+            //boven
+            if (input[lineBoven][i].ToString() == "@")
+            {
+                amountsurrounding++;
+            }
+           
+
+            if (i != 0)
+            {
+                //linksboven
+                if (input[lineBoven][indexLinks].ToString() == "@")
+                {
+                    amountsurrounding++;
+                }
+            }
+
+            if (i < input[line].Length -1) {
+                //rechtsboven
+                if (input[lineBoven][indexRechts].ToString() == "@")
+                {
+                    amountsurrounding++;
+                }
+            }
+        }
+        
+        if (line != input.Length -1 ) {
+
+            if(i != input[line].Length - 1)
+            {
+                //rechtsonder
+                if (input[lineOnder][indexRechts].ToString() == "@")
+                {
+                    amountsurrounding++;
+                }
+            }
+
+            if (i != 0)
+            {
+                //linksonder
+                if (input[lineOnder][indexLinks].ToString() == "@")
+                {
+                    amountsurrounding++;
+                }
+            }
+                   
+            //onder
+            if (input[lineOnder][i].ToString() == "@")
+            {
+                amountsurrounding++;
+            }
+        }
+
+        if (i != 0) {
+            //linksnaast
+            if (input[line][indexLinks].ToString() == "@")
+            {
+                amountsurrounding++;
+            }
+        }
+
+        if (i != input[line].Length -1 )
+        {
+            //rechtsnaast
+            if (input[line][indexRechts].ToString() == "@")
+            {
+                amountsurrounding++;
+            }
+        }
+
+        //if amountsurrounding < 4 dan amount of paper 1 meer, en als het ook een @ is
+        if (amountsurrounding < 4 )
+        {
+            amountofpaper++;
+        }
     }
-    var joltagelong = long.Parse(joltagestring);
-    joltage += joltagelong;
+   
 }
 
-Console.WriteLine($"Number of joltage: {joltage}");
+Console.WriteLine($"Number of paper: {amountofpaper}");
